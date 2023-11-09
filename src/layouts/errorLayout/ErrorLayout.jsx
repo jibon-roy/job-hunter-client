3c90cd9324b9ae90a1d2366f45d8f228d944d482
Jibon Krishna Roy <mailme.jibon@gmail.com>
2023-11-09 11:37:38 +0600

    404 not found page updated

--- src/layouts/errorLayout/ErrorLayout.jsx
+++ src/layouts/errorLayout/ErrorLayout.jsx
@@ -1,5 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import errorPage from '/error_page.jpg'
+import PrimaryButton from '../../components/button/PrimaryButton';
+import { Link } from 'react-router-dom';
 
 const ErrorLayout = () => {
     return (
@@ -8,7 +10,8 @@ const ErrorLayout = () => {
                 <title>JH | 404 Error Page !</title>
             </Helmet>
             <div className="flex h-[100vh] justify-center items-center w-full text-primary-blue">
-                <div>
+                <div className='text-center'>
+                    <Link to='/' className='mb-5'><PrimaryButton>Back to Home</PrimaryButton></Link>
                     <img src={errorPage} className='w-full' alt="" />
                 </div>
             </div>
