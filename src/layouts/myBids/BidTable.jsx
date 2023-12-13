83134507df3c1a7a77a108df19c40d4b7e510faf
jibon-roy <mailme.jibon@gmail.com>
2023-12-13 13:36:11 +0600

    probelm solved of bid table and unmuted the disabled status

--- src/layouts/myBids/BidTable.jsx
+++ src/layouts/myBids/BidTable.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 const BidTable = ({ data, value }) => {
 
     const [isDisabled, setIsDisabled] = useState(!data?.status)
+    // const [isDisabled, setIsDisabled] = useState(!data?.status)
 
 
     const handleDisabled = () => {
@@ -17,7 +18,7 @@ const BidTable = ({ data, value }) => {
             <td>{data?.employee}</td>
             <td>$ {data?.amount}</td>
             <td>{data?.deadline}</td>
-            {/* <td>{data?.status ? isDisabled ? <p className="text-green font-medium">Done!</p> : <p className="text-green font-medium">Complete</p> : <p className="text-red font-medium">Pending</p>}</td> */}
+            <td>{data?.status ? isDisabled ? <p className="text-green font-medium">Done!</p> : <p className="text-green font-medium">Complete</p> : <p className="text-red font-medium">Pending</p>}</td>
             <td><button disabled={isDisabled} onClick={handleDisabled} className={`${isDisabled ? 'bg-neutral-400 hover:bg-neutral-400' : ' hover:bg-primary-blue-hover bg-primary-blue'} px-2 py-1 text-primary-white rounded-md`}>Complete</button></td>
         </tr>
     );
